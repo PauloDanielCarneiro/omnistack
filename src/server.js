96f2d85f5536fc2fe4a8754f5afdc947b633d10a
Daniel <daniel.carneiro@rentbrella.com>
2019-10-04 22:14:30 -0300

    Change response to json format

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send("Hello World!")
+    res.json({ message: "Hello World!" })
 });
 
 app.listen(3333);
